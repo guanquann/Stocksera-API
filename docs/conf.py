60ad6b431d7fc4d5f94c37cc9d4be694bd4088d9
spartan737 <tanguanquan@yahoo.com>
2022-02-03 15:33:03 +0800

    Update readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -27,9 +27,9 @@ author = 'Guan Quan'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser',  "sphinx.ext.githubpages"]
+extensions = []
 
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
